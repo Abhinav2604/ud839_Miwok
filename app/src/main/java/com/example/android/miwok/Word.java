01eccf4d448f120d4b39079f85a75d1a68fe6509
Abhinav Kumar Singh <abhinavsngh26@gmail.com>
2018-12-20 03:42:04 +0530

    second last

--- app/src/main/java/com/example/android/miwok/Word.java
+++ app/src/main/java/com/example/android/miwok/Word.java
@@ -3,17 +3,20 @@ package com.example.android.miwok;
 public class Word {
     private String mDefaultTranslation;
     private String mMiwokTranslation;
+    private int mAudioResourceId;
     private int mImageResourceId=NO_IMAGE_PROVIDED;
     private static final int NO_IMAGE_PROVIDED = -1;
-    public Word(String DefaultTranslation, String MiwokTranslation)
-    {
-        mDefaultTranslation=DefaultTranslation;
-        mMiwokTranslation=MiwokTranslation;
+    public Word(String defaultTranslation, String miwokTranslation, int audioResourceId) {
+        mDefaultTranslation = defaultTranslation;
+        mMiwokTranslation = miwokTranslation;
+        mAudioResourceId = audioResourceId;
     }
-    public Word(String defaultTranslation, String miwokTranslation, int imageResourceId) {
+    public Word(String defaultTranslation, String miwokTranslation, int imageResourceId,
+                int audioResourceId) {
         mDefaultTranslation = defaultTranslation;
         mMiwokTranslation = miwokTranslation;
         mImageResourceId = imageResourceId;
+        mAudioResourceId = audioResourceId;
     }
 
 
@@ -27,6 +30,10 @@ public class Word {
     public int getImageResourceId() {
         return mImageResourceId;
     }
+    public int getAudioResourceId() {
+        return mAudioResourceId;
+    }
+
     public boolean hasImage() {
         return mImageResourceId != NO_IMAGE_PROVIDED;
     }
